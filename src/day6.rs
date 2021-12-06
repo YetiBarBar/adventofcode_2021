b7ef5f287f76a66e3210171b8099b67a9eb5be7c
YetiBarBar <yetibarbar@gmail.com>
2021-12-06 21:09:11 +0100

    Add: Add solution for day 6

--- src/day6.rs
+++ src/day6.rs
@@ -1,173 +1,43 @@
-use std::{collections::HashMap, fmt::Debug, str::FromStr};
+use std::{collections::VecDeque, path::PathBuf};
 
-use adventofcode_2021::{utils::read_lines, AocError};
-
-#[derive(Debug, PartialEq)]
-pub struct Point {
-    x: isize,
-    y: isize,
+struct LanternfishGroup {
+    group: VecDeque<u128>,
 }
 
-impl FromStr for Point {
-    type Err = AocError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<_> = s.split(',').collect();
-        let x = parse_part(&parts, 0)?;
-        let y = parse_part(&parts, 1)?;
-
-        Ok(Point { x, y })
+impl LanternfishGroup {
+    pub fn new() -> Self {
+        Self {
+            group: VecDeque::<u128>::with_capacity(8),
+        }
     }
-}
-
-fn parse_part<T: FromStr>(parts: &[&str], idx: usize) -> Result<T, AocError> {
-    parts
-        .get(idx)
-        .ok_or(AocError::ParsingError)?
-        .trim()
-        .parse()
-        .map_err(|_| AocError::ParsingError)
-}
-
-#[derive(Debug, PartialEq)]
-pub struct Segment {
-    pub a: Point,
-    pub b: Point,
-}
-
-impl FromStr for Segment {
-    type Err = AocError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<_> = s.split("->").collect();
-
-        let a = parse_part(&parts, 0)?;
-        let b = parse_part(&parts, 1)?;
-
-        Ok(Segment { a, b })
-    }
-}
-
-impl Segment {
-    #[must_use]
-    pub fn is_horizontal(&self) -> bool {
-        self.a.y == self.b.y
-    }
-
-    #[must_use]
-    pub fn is_vertical(&self) -> bool {
-        self.a.x == self.b.x
-    }
-
-    #[must_use]
-    pub fn is_diagonal(&self) -> bool {
-        let d_y = self.a.y - self.b.y;
-        let d_x = self.a.x - self.b.x;
-        d_y.abs() == d_x.abs()
-    }
-
-    #[must_use]
-    pub fn is_horizontal_or_vertical(&self) -> bool {
-        self.is_horizontal() || self.is_vertical()
-    }
-
-    #[must_use]
-    pub fn is_horizontal_vertical_or_diagonal(&self) -> bool {
-        self.is_diagonal() || self.is_horizontal_or_vertical()
-    }
-
-    #[must_use]
-    pub fn points(&self) -> Vec<Point> {
-        let mut res = self.h_points();
-        res.extend(self.v_points());
-        res.extend(self.diag_points());
-        res
-    }
-
-    #[must_use]
-    pub fn h_points(&self) -> Vec<Point> {
-        if self.is_horizontal() {
-            let min = self.a.x.min(self.b.x);
-            let max = self.a.x.max(self.b.x);
-            (min..=max).map(|x| Point { x, y: self.b.y }).collect()
-        } else {
-            vec![]
+    pub fn populate(&mut self, values: &[usize]) {
+        for idx in 0..=8 {
+            self.group
+                .push_back(values.iter().filter(|&&v| v == idx).count() as u128);
         }
     }
 
-    #[must_use]
-    pub fn v_points(&self) -> Vec<Point> {
-        if self.is_vertical() {
-            let min = self.a.y.min(self.b.y);
-            let max = self.a.y.max(self.b.y);
-
-            (min..=max).map(|y| Point { x: self.b.x, y }).collect()
-        } else {
-            vec![]
+    pub fn turn(&mut self) {
+        let first = self.group.pop_front().unwrap();
+        if let Some(v) = self.group.get_mut(6) {
+            *v += first;
         }
+        self.group.push_back(first);
     }
 
-    #[must_use]
-    pub fn diag_points(&self) -> Vec<Point> {
-        if self.is_diagonal() {
-            let min_x = self.a.x.min(self.b.x);
-            let max_x = self.a.x.max(self.b.x);
-            let min_y = self.a.y.min(self.b.y);
-            let max_y = self.a.y.max(self.b.y);
-
-            let p = Point { x: min_x, y: min_y };
-            if p == self.a || p == self.b {
-                (min_x..=max_x)
-                    .zip(min_y..=max_y)
-                    .map(|(x, y)| Point { x, y })
-                    .collect()
-            } else {
-                (min_x..=max_x)
-                    .zip((min_y..=max_y).rev())
-                    .map(|(x, y)| Point { x, y })
-                    .collect()
-            }
-        } else {
-            vec![]
-        }
+    pub fn result(&self) -> u128 {
+        self.group.iter().sum()
     }
 }
-/// Process data for a given step
-///
-/// # Errors
-///
-/// TBD
-pub fn part_1(data: &[Segment]) -> Result<usize, AocError> {
-    process(data, Segment::is_horizontal_or_vertical)
-}
 
-/// Process data for a given step
-///
-/// # Errors
-///
-/// TBD
-pub fn part_2(data: &[Segment]) -> Result<usize, AocError> {
-    process(data, Segment::is_horizontal_vertical_or_diagonal)
-}
-
-/// Process data for a given step
-///
-/// # Errors
-///
-/// TBD
-pub fn process(
-    data: &[Segment],
-    seg_condition: impl Fn(&Segment) -> bool,
-) -> Result<usize, AocError> {
-    let mut hmap: HashMap<_, usize> = HashMap::new();
-    data.iter()
-        .filter(|s| seg_condition(s))
-        .flat_map(Segment::points)
-        .for_each(|point| {
-            let pos = (point.x, point.y);
-            *hmap.entry(pos).or_default() += 1;
-        });
-    Ok(hmap.iter().filter(|&(_, val)| val.ge(&2)).count())
+#[must_use]
+pub fn process(values: &[usize], turns: usize) -> u128 {
+    let mut group = LanternfishGroup::new();
+    group.populate(values);
+    for _ in 0..turns {
+        group.turn();
+    }
+    group.result()
 }
 
 /// Process solutions for day 1
@@ -176,70 +46,38 @@ pub fn process(
 ///
 /// May fail if input data cannot be read
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let values: Vec<_> = read_lines("day_2021_5.data")?
-        .map(Result::unwrap)
-        .map(|line| Segment::from_str(&line))
-        .map(Result::unwrap)
-        .collect();
+    // Read file to a single string
+    let mut filepath: PathBuf = std::env::current_dir().unwrap();
+    filepath.push("data");
+    filepath.push("day_2021_6.data");
 
-    println!("Part 1: {:?}", part_1(&values));
-    println!("Part 2: {:?}", part_2(&values));
+    let input_data = std::fs::read_to_string(filepath)?;
+    let values = input_data
+        .split(',')
+        .map(|s| s.trim().parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+
+    println!("Part 1: {:?}", process(&values, 80));
+    println!("Part 2: {:?}", process(&values, 256));
+
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
     #[test]
-    fn test_day5_segment_from_str() {
-        let segment = "0,9 -> 5,9";
-        assert_eq!(
-            Segment::from_str(&segment).unwrap(),
-            Segment {
-                a: Point { x: 0, y: 9 },
-                b: Point { x: 5, y: 9 }
-            }
-        );
-    }
+    fn test_day6_part1() {
+        let values = [3_usize, 4, 3, 1, 2];
 
-    #[test]
-    fn test_day5_step1() {
-        let values = r#"0,9 -> 5,9
-8,0 -> 0,8
-9,4 -> 3,4
-2,2 -> 2,1
-7,0 -> 7,4
-6,4 -> 2,0
-0,9 -> 2,9
-3,4 -> 1,4
-0,0 -> 8,8
-5,5 -> 8,2"#;
-
-        let input: Vec<_> = values
-            .lines()
-            .map(|l| Segment::from_str(l).unwrap())
-            .collect();
-        assert_eq!(part_1(&input).unwrap(), 5);
+        assert_eq!(process(&values, 80), 5934);
     }
     #[test]
-    fn test_day5_step2() {
-        let values = r#"0,9 -> 5,9
-8,0 -> 0,8
-9,4 -> 3,4
-2,2 -> 2,1
-7,0 -> 7,4
-6,4 -> 2,0
-0,9 -> 2,9
-3,4 -> 1,4
-0,0 -> 8,8
-5,5 -> 8,2"#;
+    fn test_day6_part2() {
+        let values = [3_usize, 4, 3, 1, 2];
 
-        let input: Vec<_> = values
-            .lines()
-            .map(|l| Segment::from_str(l).unwrap())
-            .collect();
-        assert_eq!(part_2(&input).unwrap(), 12);
-        assert!(true);
+        assert_eq!(process(&values, 256), 26984457539);
     }
 }
