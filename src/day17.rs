302d4bb8eea23f1352b514a28be399b486fcb1f6
YetiBarBar <yetibarbar@gmail.com>
2021-12-17 12:28:04 +0100

    Refactor: Iterator way for day 17

--- src/day17.rs
+++ src/day17.rs
@@ -2,7 +2,7 @@ use std::{path::PathBuf, str::FromStr};
 
 use adventofcode_tooling::AocError;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Target {
     xmin: isize,
     xmax: isize,
@@ -17,15 +17,7 @@ struct Probe {
     vx: isize,
     vy: isize,
     max_height: isize,
-}
-
-impl Target {
-    fn is_in(&self, probe: &Probe) -> bool {
-        self.xmin.le(&probe.x)
-            && self.xmax.ge(&probe.x)
-            && self.ymax.ge(&probe.y)
-            && self.ymin.le(&probe.y)
-    }
+    target: Target,
 }
 
 impl FromStr for Target {
@@ -71,52 +63,78 @@ fn parse_range(input: &str) -> Result<(isize, isize), AocError> {
 }
 
 impl Probe {
-    fn step(&self, target: &Target) -> Option<Self> {
+    fn compute_next(&self) -> Option<Self> {
         let next_target = Probe {
             x: self.x + self.vx,
             y: self.y + self.vy,
             vx: self.vx - self.vx.signum(),
             vy: self.vy - 1,
             max_height: self.max_height.max(self.y + self.vy),
+            target: self.target.clone(),
         };
 
-        if next_target.vx.le(&0) && next_target.x.lt(&target.xmin) {
-            // No hope to ever reach x_min
-            return None;
+        if (next_target.vx.le(&0) && next_target.x.lt(&self.target.xmin))
+        // No more hope to reach xmin
+            || (next_target.vx.ge(&0) && next_target.x.gt(&self.target.xmax))
+            // No more hope to reach xmax
+            || (next_target.vy.lt(&0) && next_target.y.lt(&self.target.ymin))
+        // Too deep
+        {
+            None
+        } else {
+            Some(next_target)
         }
+    }
 
-        if next_target.vx.ge(&0) && next_target.x.gt(&target.xmax) {
-            // No hope to ever reach x_max
-            return None;
+    fn is_target_hit(&self) -> bool {
+        self.target.xmin.le(&self.x)
+            && self.target.xmax.ge(&self.x)
+            && self.target.ymax.ge(&self.y)
+            && self.target.ymin.le(&self.y)
+    }
+}
+
+struct ProbeIterator(Probe);
+
+impl IntoIterator for Probe {
+    type Item = Probe;
+    type IntoIter = ProbeIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+        ProbeIterator(self)
+    }
+}
+
+impl Iterator for ProbeIterator {
+    type Item = Probe;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(probe) = self.0.compute_next() {
+            self.0 = probe.clone();
+            Some(probe)
+        } else {
+            None
         }
-
-        if next_target.vy.lt(&0) && next_target.y.lt(&target.ymin) {
-            // We go deep but are already to low!
-            return None;
-        }
-
-        // We can push our new point!
-        Some(next_target)
     }
 }
 
 fn max_height(vx: isize, vy: isize, target: &Target) -> Option<isize> {
-    let mut probe = Probe {
+    let probe = Probe {
         x: 0,
         y: 0,
         vx,
         vy,
         max_height: 0,
+        target: target.clone(),
     };
 
     let mut max_h;
 
-    while let Some(new_probe) = probe.step(target) {
-        max_h = new_probe.max_height;
-        if target.is_in(&new_probe) {
+    for p in probe {
+        max_h = p.max_height;
+        if p.is_target_hit() {
             return Some(max_h);
         }
-        probe = new_probe;
     }
     None
 }
